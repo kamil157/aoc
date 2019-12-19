2df93b27e3f8d7435c5cba7fd58b583b050f2a70
Kamil <kamilchlodnicki@gmail.com>
2019-12-19 21:09:07 +0100

    Refactor day18

--- day18.py
+++ day18.py
@@ -115,129 +115,120 @@ ex5 = """########################
 ###g#h#i################
 ########################"""
 
+class MazeSolver:
+    def __init__(self, s):
+        self.cache = {}
+        self.s = s
+        self.paths = {}
+        self.map = None
+        self.results = {}
 
-def find_reachable_keys(map, pos):
-    # find shortest path to all reachable keys
-    reachable_keys = {}
+    def find_reachable_keys(self, pos):
+        # find shortest path to all reachable keys
+        reachable_keys = {}
 
-    q = collections.deque()
-    q.append((pos, 0, ""))
+        q = collections.deque()
+        q.append((pos, 0, ""))
 
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    visited = [[False] * len(map[0]) for _ in range(len(map))]
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        visited = [[False] * len(self.map[0]) for _ in range(len(self.map))]
 
-    while q:
-        (x, y), length, doors = q.popleft()
-        current = map[y][x]
-        if visited[y][x] or current == '#':
-            continue
-
-        visited[y][x] = True
-
-        if current.islower() and pos != (x, y):
-            reachable_keys[current] = (length, (x, y), doors)
-
-        if current.isupper():
-            doors += current
-
-        for d in directions:
-            next_x, next_y = x + d[0], y + d[1]
-            q.append(((next_x, next_y), length + 1, doors))
-    return reachable_keys
-
-
-def dfs(map, node, path, length, results, paths, cache):
-    # print(cache)
-    if len(path) > 0:
-        try:
-            cached = cache[(''.join(sorted(path)), path[-1], node)]
-            # print('hit: from path:', path, 'to node', node, 'subtree is', cached)
-            results[path + cached[0]] = cached[1] + length  # unsort cached[0]
-            return cached
-        except KeyError:
-            pass
-
-    current_length = 0
-    if len(path) > 0:
-        prev = path[-1]
-        current_length = paths[prev][node][0]
-        length += current_length
-    path += node
-
-    best_length = 99999
-    best_path = ''
-    if len(path) == len(paths):
-        # found all keys, done
-        results[path] = length
-        return node, current_length
-    else:
-        # check all unlocked paths
-        for key, (_, _, doors) in paths[node].items():
-            if key in path or any(door.lower() not in path for door in doors):
+        while q:
+            (x, y), length, doors = q.popleft()
+            current = self.map[y][x]
+            if visited[y][x] or current == '#':
                 continue
 
-            # print('from', node, 'to', key, 'doors:', doors, 'keys:', path[1:], 'length', length)
-            subtree_path, subtree_length = dfs(map, key, path, length, results, paths, cache)
-            if subtree_length + current_length < best_length:
-                best_length = subtree_length + current_length
-                best_path = node + subtree_path
-            # print('subtree:', subtree_path, subtree_length)
+            visited[y][x] = True
 
-    if len(path) > 1:
-        cache_key = (''.join(sorted(path[:-1])), path[-2], node)
-        cache[cache_key] = (best_path, best_length)
-        # print('best length of', best_path, '=', best_length)
-    return best_path, best_length
+            if current.islower() and pos != (x, y):
+                reachable_keys[current] = (length, (x, y), doors)
 
+            if current.isupper():
+                doors += current
 
-def collect_keys2(s):
-    """
-    find shortest path from @ to all keys
-    find shortest path from all keys to other keys
-
-    for each reachable key:
-        get this key first
-        remember total path length
-        get next keys (recurse)
-    return best total path length
-    """
-
-    # print(s)
-
-    # find all key positions
-    keys = {}
-    map = s.splitlines()
-    for y in range(len(map)):
-        for x in range(len(map[y])):
-            current = map[y][x]
-            if current == '@':
-                start = (x, y)
-            if current.islower():
-                keys[current] = (x, y)
-
-    # compute shortest paths between keys
-    paths = {}
-    reachable_keys = find_reachable_keys(map, start)
-    paths['@'] = reachable_keys
-    # for k, v in paths.items():
-    #     print(k, '=>', v)
-    for key, (x, y) in keys.items():
-        paths[key] = find_reachable_keys(map, (x, y))
-    # for k, v in paths.items():
-    #     print(k, '=>', v)
-
-    # compute stuff
-    results = {}
-    dfs(map, '@', '', 0, results, paths, {})
-    # print(results)
-    best = min(results.values())
-    print(best)
-    return best
+            for d in directions:
+                next_x, next_y = x + d[0], y + d[1]
+                q.append(((next_x, next_y), length + 1, doors))
+        return reachable_keys
 
 
-assert collect_keys2(ex1) == 8
-assert collect_keys2(ex2) == 86
-assert collect_keys2(ex3) == 132
-assert collect_keys2(ex5) == 81
-assert collect_keys2(ex4) == 136
-print(collect_keys2(input))
+    def dfs(self, node, path, length):
+        # check cache
+        if path:
+            prev = path[-1]
+            try:
+                cached_path, cached_length = self.cache[(''.join(sorted(path)), prev, node)]
+                self.results[path + cached_path] = cached_length + length
+                return (cached_path, cached_length)
+            except KeyError:
+                pass
+
+        # update state
+        current_length = 0
+        if path:            
+            current_length = self.paths[prev][node][0]
+            length += current_length
+        path += node
+
+        best_length = 99999
+        best_path = ''
+        if len(path) == len(self.paths):
+            # found all keys, done
+            self.results[path] = length
+            return node, current_length
+        else:
+            # check all unlocked paths
+            for key, (_, _, doors) in self.paths[node].items():
+                if key in path or any(door.lower() not in path for door in doors):
+                    continue
+
+                subtree_path, subtree_length = self.dfs(key, path, length)
+                if subtree_length + current_length < best_length:
+                    best_length = subtree_length + current_length
+                    best_path = node + subtree_path
+
+        if len(path) > 1:
+            cache_key = (''.join(sorted(path[:-1])), path[-2], node)
+            self.cache[cache_key] = (best_path, best_length)
+        return best_path, best_length
+
+    def solve(self):
+        """
+        find shortest path from @ to all keys
+        find shortest path from all keys to other keys
+
+        for each reachable key:
+            get this key first
+            remember total path length
+            get next keys (recurse)
+        return best total path length
+        """
+        # find all key positions
+        keys = {}
+        self.map = self.s.splitlines()
+        for y in range(len(self.map)):
+            for x in range(len(self.map[y])):
+                current = self.map[y][x]
+                if current == '@':
+                    start = (x, y)
+                if current.islower():
+                    keys[current] = (x, y)
+
+        # compute shortest paths between keys
+        reachable_keys = self.find_reachable_keys(start)
+        self.paths['@'] = reachable_keys
+        for key, (x, y) in keys.items():
+            self.paths[key] = self.find_reachable_keys((x, y))
+
+        # compute stuff
+        self.dfs(node='@', path='', length=0)
+        return min(self.results.values())
+
+
+assert MazeSolver(ex1).solve() == 8
+assert MazeSolver(ex2).solve() == 86
+assert MazeSolver(ex3).solve() == 132
+assert MazeSolver(ex5).solve() == 81
+assert MazeSolver(ex4).solve() == 136
+print(MazeSolver(input).solve())  # 5402
