d38ff7933c7d9bac860105e0d3ed85a555403e8c
Kamil <kamilchlodnicki@gmail.com>
2020-12-07 18:39:52 +0100

    Add 07/2020

---
+++ 2020/day07.py
@@ -0,0 +1,47 @@
+import re
+from collections import defaultdict, deque
+
+input = open('2020/inputs/day07.txt').readlines()
+
+def part1(s):
+    g_reversed = defaultdict(list)
+    for line in s:
+        left, right = line.split('contain')
+        left_bag = re.search(r'(\w+ \w+)', left).group(1)
+        right_bags = re.findall(r'(\w+ \w+) bag', right)
+
+        for bag in right_bags:
+            g_reversed[bag].append(left_bag)
+
+    visited = set()
+    q = deque(['shiny gold'])
+    while q:
+        node = q.popleft()
+        if node in visited:
+            continue
+        visited.add(node)
+        for neighbor in g_reversed[node]:
+            q.append(neighbor)
+
+    return len(visited) - 1
+
+def part2(s):
+    graph = {}
+    for line in s:
+        left, right = line.split('contain')
+        left_bag = re.search(r'(\w+ \w+)', left).group(1)
+        right_bags_count = re.findall(r'(\d+) (\w+ \w+) bag', right)
+        graph[left_bag] = right_bags_count
+
+    count = 0
+    q = deque([('1', 'shiny gold')])
+    while q:
+        node = q.popleft()
+        count += int(node[0])
+        for neighbor in graph[node[1]]:
+            q.append((int(node[0]) * int(neighbor[0]), neighbor[1]))
+
+    return count - 1
+
+print(part1(input))
+print(part2(input))
