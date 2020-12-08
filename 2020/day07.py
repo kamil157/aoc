ea144c53654022c14fe6042f8a2134bb48813635
Kamil <kamilchlodnicki@gmail.com>
2020-12-08 16:19:00 +0100

    Add 08/2020

--- 2020/day07.py
+++ 2020/day07.py
@@ -3,22 +3,28 @@ from collections import defaultdict, deque
 
 input = open('2020/inputs/day07.txt').readlines()
 
-def part1(s):
+def graph(s): 
+    graph = {}
     g_reversed = defaultdict(list)
     for line in s:
-        left, right = line.split('contain')
+        left, right = line.split('bags contain')
         left_bag = re.search(r'(\w+ \w+)', left).group(1)
         right_bags = re.findall(r'(\w+ \w+) bag', right)
-
+        right_bags_count = re.findall(r'(\d+) (\w+ \w+) bag', right)
+        
+        graph[left_bag] = right_bags_count
         for bag in right_bags:
             g_reversed[bag].append(left_bag)
+    
+    return g_reversed, graph
+
+def part1(s):
+    g_reversed = graph(s)[0]   
 
     visited = set()
     q = deque(['shiny gold'])
     while q:
         node = q.popleft()
-        if node in visited:
-            continue
         visited.add(node)
         for neighbor in g_reversed[node]:
             q.append(neighbor)
@@ -26,19 +32,14 @@ def part1(s):
     return len(visited) - 1
 
 def part2(s):
-    graph = {}
-    for line in s:
-        left, right = line.split('contain')
-        left_bag = re.search(r'(\w+ \w+)', left).group(1)
-        right_bags_count = re.findall(r'(\d+) (\w+ \w+) bag', right)
-        graph[left_bag] = right_bags_count
+    g = graph(s)[1]    
 
     count = 0
     q = deque([('1', 'shiny gold')])
     while q:
         node = q.popleft()
         count += int(node[0])
-        for neighbor in graph[node[1]]:
+        for neighbor in g[node[1]]:
             q.append((int(node[0]) * int(neighbor[0]), neighbor[1]))
 
     return count - 1
