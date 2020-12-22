07079f68cf1a2a481ea4076042e87473cc07f139
Kamil <kamilchlodnicki@gmail.com>
2020-12-22 14:58:04 +0100

    Add part 2 22/2020

--- 2020/day22.py
+++ 2020/day22.py
@@ -11,17 +11,48 @@ def part1(s):
 
     while cards1 and cards2:
         c1, c2 = cards1.popleft(), cards2.popleft()
-        if c1 > c2:
-            cards1.append(c1)
-            cards1.append(c2)
-        elif c2 > c1:
-            cards2.append(c2)
-            cards2.append(c1)
-        else:
-            assert False
+        winner = cards1 if c1 > c2 else cards2
+        winner.append(max(c1, c2))
+        winner.append(min(c1, c2))
 
-    winner = reversed(cards1 if cards1 else cards2)
-    return sum(i * card for i, card in enumerate(winner, 1))
+    return points(cards1 if cards1 else cards2)
+
+def serialize(cards1, cards2):
+    return tuple(cards1), tuple(cards2)
+
+def points(cards):
+    return sum(i * card for i, card in enumerate(reversed(cards), 1))
+
+def recursive(cards1, cards2):
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+
+    visited = set()
+    while cards1 and cards2:
+        if serialize(cards1, cards2) in visited:
+            return (1, cards1)
+        
+        visited.add(serialize(cards1, cards2))
+        c1, c2 = cards1.popleft(), cards2.popleft()
+        
+        if len(cards1) >= c1 and len(cards2) >= c2:
+            winner = recursive([cards1[i] for i in range(c1)],
+                               [cards2[i] for i in range(c2)])[0]
+        else:    
+            winner = 1 if c1 > c2 else 2
+
+        winner_cards = cards1 if winner == 1 else cards2
+        winner_cards.append(c1 if winner == 1 else c2)
+        winner_cards.append(c2 if winner == 1 else c1)
+
+    return (1, cards1) if cards1 else (2, cards2)
+
+def part2(s):
+    p1, p2 = s.split('\n\n')
+    cards1 = [int(card) for card in p1.splitlines()[1:]]
+    cards2 = [int(card) for card in p2.splitlines()[1:]]
+    return points(recursive(cards1, cards2)[1])
 
 
 print(part1(input))
+print(part2(input))
