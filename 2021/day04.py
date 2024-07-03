358a308a99e58fb33349be99de106b948ea60d00
Kamil <kamilchlodnicki@gmail.com>
2024-07-03 09:03:30 +0200

    2021/04

--- 2021/day04.py
+++ 2021/day04.py
@@ -1,18 +1,35 @@
-input = open('2021/inputs/day04.txt').read().split('\n\n')
-
-def part1(s):
-    nums = s[0]
-    print(nums)
-
-    boards = s[1:]
-
-    for board in boards:
-        print(board, end='\n\n')
+with open('2021/inputs/day04.txt', encoding="utf-8") as f:
+    lines = f.read().split('\n\n')
 
 
-    boards = [[[int(n) for n in line.split()] for line in board.split('\n')] for board in boards ]
-    for board in boards:
-        print(board, end='\n')
+def bingo(called, board):
+    def line_win(b, i):
+        return all(b[i][j] in called for j in range(size))
+
+    size = len(board)
+    return any(line_win(board, i) or line_win(list(zip(*board)), i) for i in range(size))
 
 
-print(part1(input))
+def score(called, board):
+    return sum(n for row in board for n in row if n not in called)
+
+
+def part1(index):
+    nums = [int(n) for n in lines[0].split(",")]
+    boards = [[[int(n) for n in line.split()]
+               for line in board.split('\n')] for board in lines[1:]]
+
+    wins = []
+    called = set()
+    for n in nums:
+        called.add(n)
+        for board in boards:
+            if bingo(called, board):
+                wins.append(n * score(called, board))
+                boards.remove(board)
+
+    return wins[index]
+
+
+print(part1(0))
+print(part1(-1))
