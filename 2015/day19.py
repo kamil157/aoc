d915eb05b3af47b49f39b49d9500c3b1de7b8b5c
Kamil <kamilchlodnicki@gmail.com>
2024-07-15 15:20:34 +0200

    2015/19 part2

--- 2015/day19.py
+++ 2015/day19.py
@@ -2,16 +2,36 @@ from re import findall, finditer
 
 input = open('2015/inputs/day19.txt').read()
 
+
 def replace(molecule, match, mol_out):
     return molecule[:match.start()] + mol_out + molecule[match.end():]
 
+
 def part1(s):
-    replacements_data = findall(r'(\w+) => (\w+)', s)
-    replacements = [(mol_in, mol_out) for mol_in, mol_out in replacements_data]
+    replacements = findall(r'(\w+) => (\w+)', s)
     molecule = s.split()[-1]
 
-    return len(set(replace(molecule, m, mol_out) 
-            for mol_in, mol_out in replacements 
-            for m in finditer(mol_in, molecule)))
+    return len(set(replace(molecule, m, mol_out)
+                   for mol_in, mol_out in replacements
+                   for m in finditer(mol_in, molecule)))
+
 
 print(part1(input))
+
+
+def part2(s):
+    replacements = findall(r'(\w+) => (\w+)', s)
+    molecule = s.split()[-1]
+
+    count = 0
+    while molecule != "e":
+        for mol_in, mol_out in replacements:
+            for m in finditer(mol_out, molecule):
+                molecule = replace(molecule, m, mol_in)
+                count += 1
+                break
+
+    return count
+
+
+print(part2(input))
