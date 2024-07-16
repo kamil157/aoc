7ccfa70035d5402668f7260b6d139613d0540ca5
Kamil <kamilchlodnicki@gmail.com>
2024-07-16 16:28:49 +0200

    2016/19

--- 2016/day19.py
+++ 2016/day19.py
@@ -1,29 +1,33 @@
 input = 3014387
 
-# def part1(s):
-#     elves = {i: (i + 1) % s for i in range(s)}
-#     i = 0
-#     while len(elves) > 1:
-#         target = elves[i]
-#         elves[i] = elves[target]
-#         del elves[target]
-#         i = elves[i]
-#     return next(iter(elves)) + 1
 
 def part1(s):
     return int(bin(s)[3:] + bin(s)[2], 2)
 
-# def part2(s):
-    # elves = {i: (i + s // 2) % s for i in range(s)}
-    # i = 0
-    # target = (i + len(elves) // 2) % len(elves)
-    # while len(elves) > 1:
-    #     print(elves)
-    #     target = elves[i]
-    #     elves[i] = (i + len(elves) // 2) % len(elves)
-    #     del elves[target]
-    #     i = elves[i]
-    # return next(iter(elves)) + 1
 
 print(part1(input))
-# print(part2(5))
+
+
+# def part2(n):
+#     elves = list(range(1, n + 1))
+#     while len(elves) > 1:
+#         print(elves)
+#         target = len(elves) // 2
+#         elves.pop(target)
+#         elves = elves[1:] + elves[:1]
+#     return elves[0]
+
+def part2(n):
+    if n == 1:
+        return 1
+    k = 1
+    while k * 3 < n:
+        k *= 3
+    if n > 2 * k:
+        guess = 2 * n - 3 * k
+    else:
+        guess = n - k
+    return guess
+
+
+print(part2(input))
