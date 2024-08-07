4ea7979ceddace69325d94aed7cfe15aa539da73
Kamil <kamilchlodnicki@gmail.com>
2024-08-07 10:55:05 +0200

    2020/23 part2

--- 2020/day23.py
+++ 2020/day23.py
@@ -26,3 +26,40 @@ def part1():
 
 
 print(part1())
+
+
+def part2():
+    cups = [int(n) for n in "137826495"]
+
+    a = {}
+    for i in range(1, len(cups) + 1):
+        a[i] = cups[(cups.index(i) + 1) % len(cups)]
+
+    count = 1000000
+    for i in range(10, count):
+        a[i] = i + 1
+    a[int(cups[-1])] = 10
+    a[count] = int(cups[0])
+
+    current = int(cups[0])
+    for i in range(10000000):
+        p1 = a[current]
+        p2 = a[p1]
+        p3 = a[p2]
+        next = a[p3]
+        pickup = [p1, p2, p3]
+        destination = current - 1
+        while destination in pickup or destination == 0:
+            if destination == 0:
+                destination = count
+            else:
+                destination -= 1
+        a[current] = next
+        a[p3] = a[destination]
+        a[destination] = p1
+        current = a[current]
+
+    return a[1] * a[a[1]]
+
+
+print(part2())
