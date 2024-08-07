381e5ec62e490f882fd27ce59b6ba8d4e5ea5a07
Kamil <kamilchlodnicki@gmail.com>
2024-08-07 16:00:09 +0200

    2021/22 part2

--- 2021/day22.py
+++ 2021/day22.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from parse import search
 
 with open('2021/inputs/day22.txt', encoding="utf-8") as f:
@@ -22,3 +23,28 @@ def part1():
 
 
 print(part1())
+
+
+def part2():
+    cubes = Counter()
+    for line in lines:
+        current = Counter()
+        sign = 1 if line.startswith("on") else -1
+        cube = search("x={:d}..{:d},y={:d}..{:d},z={:d}..{:d}", line)
+
+        for prev, prev_sign in cubes.items():
+            i = (
+                max(prev[0], cube[0]), min(prev[1], cube[1]),
+                max(prev[2], cube[2]), min(prev[3], cube[3]),
+                max(prev[4], cube[4]), min(prev[5], cube[5])
+            )
+            if i[0] <= i[1] and i[2] <= i[3] and i[4] <= i[5]:
+                current[i] -= prev_sign
+        if sign == 1:
+            current[cube] += sign
+        cubes.update(current)
+
+    return sum((c[1] - c[0] + 1) * (c[3] - c[2] + 1) * (c[5] - c[4] + 1) * count for c, count in cubes.items())
+
+
+print(part2())
