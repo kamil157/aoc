b21e539069cc8e06966cec5a1bc12f6ddec8896b
Kamil <kamilchlodnicki@gmail.com>
2024-08-09 13:25:07 +0200

    wip

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,50 +1,46 @@
 
 
-with open('2023/inputs/day10.txt') as f:
+from collections import deque
+
+
+with open('2023/inputs/day10.txt', encoding="utf-8") as f:
     lines = f.read().strip().split('\n')
 
 
-def neighbors(y, x, lines):
-    def helper(y, x, lines):
+def neighbors(y, x):
+    def helper(y, x):
         up, down, left, right = (y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)
 
-        if lines[y][x] == 'S':
-            return {up, down, left, right}
-        if lines[y][x] == '|':
-            return {up, down}
-        if lines[y][x] == '-':
-            return {left, right}
-        if lines[y][x] == 'L':
-            return {up, right}
-        if lines[y][x] == 'J':
-            return {left, up}
-        if lines[y][x] == '7':
-            return {left, down}
-        if lines[y][x] == 'F':
-            return {down, right}
-        if lines[y][x] == '.':
-            return {}
+        match lines[y][x]:
+            case 'S': return {up, down, left, right}
+            case '|': return {up, down}
+            case '-': return {left, right}
+            case 'L': return {up, right}
+            case 'J': return {left, up}
+            case '7': return {left, down}
+            case 'F': return {down, right}
+            case '.': return {}
 
     valid = set()
-    for neighbor in helper(y, x, lines):
-        if (y, x) in helper(*neighbor, lines):
+    for neighbor in helper(y, x):
+        if (y, x) in helper(*neighbor):
             valid.add(neighbor)
     return valid
 
 
 def part1():
-    queue = []
-    for y in range(len(lines)):
-        for x in range(len(lines[y])):
-            if lines[y][x] == 'S':
+    queue = deque()
+    for y, line in enumerate(lines):
+        for x, tile in enumerate(line):
+            if tile == 'S':
                 queue.append((y, x, 0))
 
     distances = {}
     while queue:
-        y, x, dist = queue.pop(0)
+        y, x, dist = queue.popleft()
         distances[(y, x)] = dist
 
-        for neighbor in neighbors(y, x, lines):
+        for neighbor in neighbors(y, x):
             if neighbor not in distances:
                 queue.append((*(neighbor), dist + 1))
 
