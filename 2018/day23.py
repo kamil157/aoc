8c259a1bff867170411c666bda73866b9320223c
Kamil <kamilchlodnicki@gmail.com>
2024-08-13 16:45:20 +0200

    2018/23 part2

--- 2018/day23.py
+++ 2018/day23.py
@@ -1,3 +1,4 @@
+import z3
 from operator import itemgetter
 from parse import findall
 
@@ -12,3 +13,34 @@ def part1():
 
 
 print(part1())
+
+
+def z3abs(x):
+    return z3.If(x >= 0, x, -x)
+
+
+def z3dist(p1, p2):
+    x1, y1, z1 = p1
+    x2, y2, z2 = p2
+    return z3abs(x1 - x2) + z3abs(y1 - y2) + z3abs(z1 - z2)
+
+
+def part2():
+    nanobots = list(findall("pos=<{:d},{:d},{:d}>, r={:d}", lines))
+
+    solver = z3.Optimize()
+    pos = z3.IntVector('pos', 3)
+    in_range = z3.IntVector('in_range', len(nanobots))
+    dist = z3.Int('dist')
+
+    for i, bot in enumerate(nanobots):
+        solver.add(in_range[i] == z3.If(z3dist(pos, bot[:3]) <= bot[3], 1, 0))
+
+    solver.maximize(z3.Sum(*in_range))
+    solver.add(dist == z3dist(pos, (0, 0, 0)))
+    solver.minimize(dist)
+    solver.check()
+    return solver.model().evaluate(dist)
+
+
+print(part2())
